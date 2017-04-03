20c1e4d5795e5f2aca2d68309adabbc6f6874d9b
Eden Rohatensky <edenrohatensky@gmail.com>
2017-04-03 17:23:08 -0400

    Fleshed out the encrypt action in the crypto controller

--- app/controllers/crypto_controller.rb
+++ app/controllers/crypto_controller.rb
@@ -1,5 +1,30 @@
 class CryptoController < ApplicationController
   def encrypt
+    text = params[:text]
+    shift = params[:shift]
+
+    text.upcase!
+    alphabet = ('A'...'Z').to_a
+
+    # Split text into characters
+    # Loop through the characters
+    cipher = text.split('').map do |letter|
+      # - Get the current index of the alphabet for the char
+      index = alphabet.index(letter)
+
+      if index
+        # - Shift the index by the shift value
+        new_index = (index + shift)
+        # - Return the new letter
+        alphabet[new_index]
+      else
+        # - If the current character isn't in the alphabet, we'll just return the char
+        letter
+      end
+    end
+
+    # Join the new characters as a string
+    @encrypted_message = cipher.join
   end
 
   def decrypt
